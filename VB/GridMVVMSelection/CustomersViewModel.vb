99b7abd8adc9258aa20353d501dc7c9b64f52b2f
DevExpressExampleBot <fakeEmail>
2020-12-28 20:17:54 +0300

    Source auto update [skip ci]

---
+++ VB/GridMVVMSelection/CustomersViewModel.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Collections.ObjectModel
+Imports DevExpress.Mvvm
+
+Namespace WPFGridMVVMSelection
+	Public Class CustomersViewModel
+		Inherits ViewModelBase
+
+		Public Sub New()
+			DeleteSelectedRowsCommand = New DelegateCommand(AddressOf DeleteSelectedRows, AddressOf CanDeleteSelectedRows)
+		End Sub
+		Public ReadOnly Property Customers() As IList(Of Customer) = CustomersDataModel.GetCustomers()
+		Public ReadOnly Property Selection() As New ObservableCollection(Of Customer)()
+		Private privateDeleteSelectedRowsCommand As DelegateCommand
+		Public Property DeleteSelectedRowsCommand() As DelegateCommand
+			Get
+				Return privateDeleteSelectedRowsCommand
+			End Get
+			Private Set(ByVal value As DelegateCommand)
+				privateDeleteSelectedRowsCommand = value
+			End Set
+		End Property
+		Private Sub DeleteSelectedRows()
+			Selection.ToList().ForEach(Function(item) Customers.Remove(item))
+		End Sub
+		Private Function CanDeleteSelectedRows() As Boolean
+			Return Selection.Count > 0
+		End Function
+	End Class
+End Namespace
---
+++ VB/GridMVVMSelection/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Windows
+
+Namespace WPFGridMVVMSelection
+	Partial Public Class MainWindow
+		Inherits Window
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
