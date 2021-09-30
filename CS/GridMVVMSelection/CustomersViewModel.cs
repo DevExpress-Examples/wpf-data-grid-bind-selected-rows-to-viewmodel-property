b7bbef0f0c6b6be81233a841187e421dc9be0107
volkov.anton <anton.volkov@devexpress.com>
2021-09-30 15:13:55 +0300

    Use auto-generated command.

--- CS/GridMVVMSelection/CustomersViewModel.cs
+++ CS/GridMVVMSelection/CustomersViewModel.cs
@@ -2,19 +2,18 @@
 using System.Linq;
 using System.Collections.ObjectModel;
 using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
 
 namespace WPFGridMVVMSelection {
     public class CustomersViewModel : ViewModelBase {
-        public CustomersViewModel() {
-            DeleteSelectedRowsCommand = new DelegateCommand(DeleteSelectedRows, CanDeleteSelectedRows);
-        }
         public IList<Customer> Customers { get; } = CustomersDataModel.GetCustomers();
         public ObservableCollection<Customer> Selection { get; } = new ObservableCollection<Customer>();
-        public DelegateCommand DeleteSelectedRowsCommand { get; private set; }
-        void DeleteSelectedRows() {
+        
+        [Command]
+        public void DeleteSelectedRows() {
             Selection.ToList().ForEach(item => Customers.Remove(item));
         }
-        bool CanDeleteSelectedRows() {
+        public bool CanDeleteSelectedRows() {
             return Selection.Count > 0;
         }
     }
