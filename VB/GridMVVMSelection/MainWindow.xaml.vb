0543499a405835e32cb19e09f0b3a35ebeaadb4a
DevExpressExampleBot <fakeEmail>
2021-11-12 15:00:13 +0300

    Source auto update [skip ci]

--- VB/GridMVVMSelection/MainWindow.xaml.vb
+++ VB/GridMVVMSelection/MainWindow.xaml.vb
@@ -1,17 +1,17 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports Model
 
 Namespace WPFGridMVVMSelection
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
             InitializeComponent()
             DataContext = New ViewModel()
         End Sub
-
     End Class
 End Namespace
--- VB/GridMVVMSelection/ViewModel.vb
+++ VB/GridMVVMSelection/ViewModel.vb
@@ -1,88 +1,105 @@
-﻿Imports System
 Imports System.Collections.ObjectModel
 Imports System.Windows.Input
 Imports DevExpress.Utils.Commands
 Imports DevExpress.Xpf.Core.Commands
-Imports System.Collections
 Imports System.Linq
 Imports DevExpress.Xpf.Core
 Imports System.Collections.Specialized
 
 Namespace Model
+
     Public Class ViewModel
 
-        Private ReadOnly source_Renamed As ObservableCollection(Of DataItem) = DataItem.GetData()
+        Private _SelectOddRowsCommand As ICommand, _DeleteSelectedRowsCommand As DelegateCommand(Of Object)
 
-        Private ReadOnly selection_Renamed As New ObservableCollection(Of DataItem)()
-        Public ReadOnly Property Source() As ObservableCollection(Of DataItem)
+        Private ReadOnly sourceField As ObservableCollection(Of DataItem) = DataItem.GetData()
+
+        Private ReadOnly selectionField As ObservableCollection(Of DataItem) = New ObservableCollection(Of DataItem)()
+
+        Public ReadOnly Property Source As ObservableCollection(Of DataItem)
             Get
-                Return Me.source_Renamed
+                Return sourceField
             End Get
         End Property
-        Public ReadOnly Property Selection() As ObservableCollection(Of DataItem)
+
+        Public ReadOnly Property Selection As ObservableCollection(Of DataItem)
             Get
-                Return Me.selection_Renamed
+                Return selectionField
             End Get
         End Property
-        Private privateSelectOddRowsCommand As ICommand
-        Public Property SelectOddRowsCommand() As ICommand
+
+        Public Property SelectOddRowsCommand As ICommand
             Get
-                Return privateSelectOddRowsCommand
+                Return _SelectOddRowsCommand
             End Get
+
             Private Set(ByVal value As ICommand)
-                privateSelectOddRowsCommand = value
+                _SelectOddRowsCommand = value
             End Set
         End Property
-        Private privateDeleteSelectedRowsCommand As DelegateCommand(Of Object)
-        Public Property DeleteSelectedRowsCommand() As DelegateCommand(Of Object)
+
+        Public Property DeleteSelectedRowsCommand As DelegateCommand(Of Object)
             Get
-                Return privateDeleteSelectedRowsCommand
+                Return _DeleteSelectedRowsCommand
             End Get
+
             Private Set(ByVal value As DelegateCommand(Of Object))
-                privateDeleteSelectedRowsCommand = value
+                _DeleteSelectedRowsCommand = value
             End Set
         End Property
+
         Public Sub New()
             AddHandler Selection.CollectionChanged, AddressOf Selection_CollectionChanged
-
             SelectOddRowsCommand = New DelegateCommand(Of Object)(AddressOf SelectOddRows)
             DeleteSelectedRowsCommand = New DelegateCommand(Of Object)(AddressOf DeleteSelectedRows, AddressOf CanDeleteSelectedRows)
         End Sub
+
         Private Sub Selection_CollectionChanged(ByVal sender As Object, ByVal e As NotifyCollectionChangedEventArgs)
             DeleteSelectedRowsCommand.RaiseCanExecuteChanged()
         End Sub
+
         Private Sub SelectOddRows(ByVal obj As Object)
             Selection.Clear()
             For i As Integer = 0 To Source.Count - 1 Step 2
                 Selection.Add(Source(i))
-            Next i
+            Next
         End Sub
+
         Private Sub DeleteSelectedRows(ByVal obj As Object)
-            Selection.ToList().ForEach(Function(item) Source.Remove(item))
+            Enumerable.ToList(Selection).ForEach(Sub(item) Source.Remove(item))
         End Sub
+
         Private Function CanDeleteSelectedRows(ByVal obj As Object) As Boolean
             Return Selection.Count > 0
         End Function
     End Class
 
     Public Class DataItem
-        Public Shared Function GetData(Optional ByVal count As Integer = 100) As ObservableCollection(Of DataItem)
-            Dim result As New ObservableCollection(Of DataItem)()
+
+        Public Shared Function GetData(ByVal Optional count As Integer = 100) As ObservableCollection(Of DataItem)
+            Dim result As ObservableCollection(Of DataItem) = New ObservableCollection(Of DataItem)()
             For i As Integer = 0 To count - 1
                 result.Add(New DataItem(i, "Name" & i, Date.Now.AddDays(i)))
-            Next i
+            Next
+
             Return result
         End Function
-        Public Property Id() As Integer
-        Public Property Name() As String
-        Public Property Date_Time() As Date
+
+        Public Property Id As Integer
+
+        Public Property Name As String
+
+        Public Property Date_Time As Date
+
         Public Sub New()
         End Sub
+
         Public Sub New(ByVal id As Integer, ByVal name As String, ByVal [date] As Date)
             Me.Id = id
             Me.Name = name
             Date_Time = [date]
         End Sub
+
         Public Overrides Function ToString() As String
             Return Name
         End Function
